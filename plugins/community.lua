f44cb5369c20475723c8d330fef053ec8323c63f
chaibowen <150130030@qq.com>
2024-03-02 23:28:55 +0800

    plugins for python, c++

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,30 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+
+  {
+    "compiler.nvim",
+    keys = {
+      { "<leader>r", "", desc = "Compiler" },
+      { "<leader>ro", "<cmd>CompilerOpen<cr>", desc = "CompilerOpen" },
+      { "<leader>rt", "<cmd>CompilerToggleResult<cr>", desc = "CompilerToggleResult" },
+    },
+  },
+
+  {
+    "noice.nvim",
+    config = function()
+      require("noice").setup {
+        presets = {
+          command_palette = false, -- position the cmdline and popupmenu together
+        },
+      }
+    end,
+  },
 }
---
+++ plugins/nvim-notify.lua
@@ -0,0 +1,11 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    stages = "static",
+    render = "compact",
+    max_width = "50",
+    fps = 5,
+    level = 1,
+    timeoout = 3000,
+  },
+}
